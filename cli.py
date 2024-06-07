9893a3696a000e9f74d8e9354dfa3460695b33af
Han HU <huhanwj@gmail.com>
2024-06-07 14:53:51 +0800

    rtsp stream

--- cli.py
+++ cli.py
@@ -10,8 +10,13 @@ tokenizer = AutoTokenizer.from_pretrained('openbmb/MiniCPM-Llama3-V-2_5', trust_
 model.eval()
 
 # Open the video stream from the remote side
-video_stream_url = 'rtsp://example.com/video_stream'  # Replace with the actual URL of the remote video stream
+video_stream_url = 'rtsp://192.168.50.70/live'  # Replace with the actual URL of the remote video stream
 video_capture = cv2.VideoCapture(video_stream_url)
+image_path = input("Image path (or press Enter to skip): ")
+if image_path:
+    image = Image.open(image_path).convert('RGB')
+else:
+    image = None
 
 while True:
     question = input("User: ")
@@ -21,14 +26,16 @@ while True:
     # Capture the current frame from the video stream
     ret, frame = video_capture.read()
     if not ret:
-        print("Failed to capture frame from the video stream.")
-        break
+        print("Failed to capture frame from the video stream. Use normal picture instead!")
+        image = Image.open(image_path).convert('RGB')
+        # break
+    else:
 
-    # Convert the frame from BGR to RGB format
-    frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        # Convert the frame from BGR to RGB format
+        frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-    # Convert the frame to PIL Image format
-    image = Image.fromarray(frame_rgb)
+        # Convert the frame to PIL Image format
+        image = Image.fromarray(frame_rgb)
 
     msgs = [{'role': 'user', 'content': question}]
 
